c7d7d9c4821cf34da99ee07210fe44207fb81c07
Gage <gage.bonner@gmail.com>
2024-02-28 16:56:56 -0500

    another simplification

--- practice.jl
+++ practice.jl
@@ -9,16 +9,15 @@ theta = [1 1 1;
 dXdt = [2.3; 6.1; 11.7; 20] # x^2 + x w/ added noise
 
 lambda = 0.2 # sparcification parameter
-n = 1
 Xi = theta \ dXdt
 println("before: ", Xi)
+
 for k in 1:10
     smallinds = findall(<(lambda), abs.(Xi)) #array of indicies with small coefficients
     Xi[smallinds] .= 0
-    for ind in 1:n
-        biginds = [i for i = 1:length(Xi) if !(i in smallinds)]
-        Xi[biginds, ind] = theta[:, biginds] \ dXdt[:, ind]
-    end
+    biginds = [i for i = 1:length(Xi) if !(i in smallinds)]
+    
+    Xi[biginds] = theta[:, biginds] \ dXdt
 end
 
 print("after: ", Xi)
