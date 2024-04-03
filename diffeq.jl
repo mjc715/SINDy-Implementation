94ab7ccbe0b320e168fa08f0841791992857bdad
Michael Castellucci <mjc715@miami.edu>
2024-04-03 16:29:46 -0400

    changes

--- diffeq.jl
+++ diffeq.jl
@@ -1,6 +1,10 @@
 using DifferentialEquations
 using ForwardDiff
 
+f1(t) = t
+f2(t) = t^2
+f3(t) = t^3
+
 function f!(du, u, p, t)
     du[1] = 2t + t^2
 end
@@ -8,17 +12,45 @@ end
 prob = ODEProblem(f!, [0.0], (0.0, 10.0))
 sol = solve(prob) # function you can evaluate, like sol(3.0)
 
-times = range(0.0, 10.0, length = 5) |> collect
+times = range(0.0, 1.0, length=10) |> collect
 data = [ForwardDiff.derivative(t -> sol(t)[1], ti) for ti in times]
 
 ### multivariable, vector output
 function f_vec!(du, u, p, t)
     du[1] = 2u[1] - u[2]^2
-    du[2] = u[1]*u[2] + 3
+    du[2] = u[1] * u[2] + 3
 end
 
 prob_vec = ODEProblem(f_vec!, [0.0, 0.0], (0.0, 1.0))
 sol_vec = solve(prob_vec) # function you can evaluate, like sol(3.0)
 
-times = range(0.0, 1.0, length = 5) |> collect
 data = [ForwardDiff.derivative(t -> sol_vec(t), ti) for ti in times]
+
+times = [times; times]
+
+n_iterations = 10
+row_1 = [1 for t in times]
+row_x = [f1(t) for t in times]
+row_y = [f1(2t) for t in times]
+row_xy = [f1(t) * f1(2t) for t in times]
+row_y2 = [f2(2t) for t in times]
+theta = [row_1;; row_x;; row_y;; row_xy;; row_y2]
+row3 = theta[1:10, :]
+row4 = theta[11:20, :]
+thetas = [[row3] [row4]]
+data1 = [data[i][1] for i in 1:length(data)]
+data2 = [data[i][2] for i in 1:length(data)]
+datas = [data1, data2]
+lambda = 0.25
+for l in 1:size(datas, 1)
+    Xi = thetas[l] \ datas[l]
+    for k in 1:n_iterations
+        smallinds = findall(p -> (p < abs(lambda)), Xi) #array of indicies with small coefficients
+        # println(smallinds)
+        Xi[smallinds] .= 0
+        biginds = [i for i = 1:length(Xi) if !(i in smallinds)]
+        # println(biginds)
+        Xi[biginds] = thetas[l][:, biginds] \ datas[l]
+    end
+    println(l, ": ", Xi)
+end
--- practice.jl
+++ practice.jl
@@ -1,17 +1,91 @@
 # Sparse representation algorithm
 #####################################
-
+f0(x) = 1
 f1(x) = x
 f2(x) = x^2
 f3(x) = x^3
-f4(y) = y
+f4(y) = (2y)
+f5(y) = (2y^2)
+f6(y) = (2y^3)
+f7(x) = f1(x) * f4(x)
+f8(x) = f1(x) * f5(x)
+f9(x) = f2(x) * f4(x)
+f10(x) = f2(x) * f5(x)
 
-n_iterations = 10
-n_vars = 1
-# data = [
-#     0 1 2 3 4 5 6 7 8 9 10;
-#     0 2 4 6 8 10 12 14 16 18 20
-# ] # x,2y
+library = [f0, f1, f2, f4, f7]
+times = [i for i = 1:10]
+vars = 2
+data = [
+    0 2 6 12 20 30 42 56 72 90;
+    5 11 21 35 53 75 101 131 165 203
+] # 1. x^2-x
+# 2. xy + 3 (x = t, y = 2t)
+function sparse_representation(library, data, times, vars)
+    helper = []
+    n_iterations = 10
+    lambda = 0.25
+
+    for i in 1:vars-1
+        times = [times; times]
+    end
+
+    for f in library
+        i = 1
+        row = zeros(length(times))
+        for t in times
+            row[i] = f(t)
+            i += 1
+        end
+        push!(helper, row)
+
+    end
+
+    theta = zeros(length(times), length(library))
+    dims = size(theta)
+
+    for c in 1:dims[2]
+        for r in 1:dims[1]
+            theta[r, c] = helper[c][r]
+        end
+    end
+
+    data = vec(permutedims(data))
+    # print(data)
+    data_arrays = []
+    theta_arrays = []
+
+    if vars == 2
+        midpoint = Integer(length(data) / 2)
+        row1 = data[1:midpoint]
+        row2 = data[midpoint+1:length(data)]
+        row3 = theta[1:midpoint, :]
+        row4 = theta[(midpoint+1:length(data)), :]
+        data_arrays = [[row1] [row2]]
+        theta_arrays = [[row3] [row4]]
+    elseif vars == 1
+        data_arrays = data
+        theta_arrays = theta
+    end
+
+
+    for l in 1:vars
+        Xi = theta_arrays[l] \ data_arrays[l]
+        # print(Xi)
+        for k in 1:n_iterations
+            smallinds = findall(p -> (p < abs(lambda)), Xi) #array of indicies with small coefficients
+            # println(smallinds)
+            Xi[smallinds] .= 0
+            biginds = [i for i = 1:length(Xi) if !(i in smallinds)]
+            # println(biginds)
+            Xi[biginds] = theta_arrays[l][:, biginds] \ data_arrays[l]
+        end
+        println(l, ": ", Xi)
+    end
+
+end
+
+theta = sparse_representation(library, data, times, vars)
+
 
 ########### Scalar Multivariable ####################
 # data = [
@@ -47,8 +121,8 @@ data = [
 times = [[i for i = 0:10]; [i for i = 0:10]]
 row_1 = [1 for t in times]
 row_x = [f1(t) for t in times]
-row_y = [f4(2t) for t in times]
-row_xy = [f1(t) * f4(2t) for t in times]
+row_y = [f4(t) for t in times]
+row_xy = [f1(t) * f4(t) for t in times]
 row_x3 = [f3(t) for t in times]
 
 theta = [row_1;; row_x;; row_y;; row_xy;; row_x3]
@@ -74,13 +148,10 @@ for l in 1:size(datas, 2)
     println(l, ": ", Xi)
 end
 ######################################################
-
-
-
 ###################################### make theta vector of matricies w/ each matrix like eq 4a
 ### plan
-# 1. write function for 1 variable case
-# 2. put ind back in to get multivariable case (vectorize)
+# 1. clean up, make funtion with few inputs (data & library)
+# 2. 
 # 3. true SINDy
 
 # scalar multivariable case
